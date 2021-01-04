50f77b2a6b3b4b870d93dc5a54a9ee53c0714994
bsmrvl <ben.j.somerville@icloud.com>
2021-01-04 16:02:53 -0500

    slight wrangler implementation edit

--- airbnb_app/app.py
+++ airbnb_app/app.py
@@ -1,9 +1,9 @@
 """Main functions and routes for Airbnb app."""
 
-from os import getenv
+import os
 from flask import Flask, request, render_template, redirect, url_for
 from flask_sqlalchemy import SQLAlchemy
-from werkzeug import secure_filename
+from werkzeug.utils import secure_filename
 
 from .wrangler import wrangle_image
 
@@ -14,7 +14,7 @@ def create_app():
     """Construct app and it's routes."""
     app = Flask(__name__)
 
-    app.config["SQLALCHEMY_DATABASE_URI"] = getenv("DATABASE_URL")
+    app.config["SQLALCHEMY_DATABASE_URI"] = os.getenv("DATABASE_URL")
     app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
     DB.init_app(app)
 
@@ -30,7 +30,7 @@ def create_app():
     def upload_post():
         if request.method == "POST":
             img = request.files["file"]
-            orig_dir = "images/original"+str(secure_filename(img.filename))
+            orig_dir = os.path.join('images/original', str(secure_filename(img.filename)))
             img.save(orig_dir)
             new_dir = "images/resized/"
             wrangle_image(orig_dir, new_dir)
