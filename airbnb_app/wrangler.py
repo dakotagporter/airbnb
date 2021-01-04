35526ff32f183c09951f45b40eab1821ec117ea9
dakotagporter <dakotap3045@gmail.com>
2021-01-04 14:50:22 -0700

    add requirements

--- airbnb_app/wrangler.py
+++ airbnb_app/wrangler.py
@@ -2,8 +2,9 @@ import os
 from skimage import io, img_as_ubyte, color
 from skimage.transform import resize
 
+
 def wrangle_image(orig_dir, new_dir):
-    image = resize(io.imread(orig_dir), (180,240), mode='constant', anti_aliasing=True)
+    image = resize(io.imread(orig_dir), (180, 240), mode='constant', anti_aliasing=True)
 
     if len(image.shape) < 3:
         image = color.grey2rgb(image)
