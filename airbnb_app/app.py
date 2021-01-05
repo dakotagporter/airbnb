63424f8c4b4eab45feee64a24ede039478417a67
dakotagporter <dakotap3045@gmail.com>
2021-01-05 10:41:10 -0700

    fix image functionality

--- airbnb_app/app.py
+++ airbnb_app/app.py
@@ -88,10 +88,10 @@ def create_app():
         Evaluate user input to return estimate.
         """
         if request.method == "POST":
-            #amens = request.form["amenities"]
-            #if not amens:
-            #    flash("Select at least one amenity")
-            #    return redirect(url_for("upload"))
+            amens = request.form["amenities"]
+            if not amens:
+                flash("Please select at least one amenity")
+                return redirect(url_for("upload"))
 
             img = request.files.get("file", False)
             if not img:
@@ -105,7 +105,6 @@ def create_app():
                 new_dir = "images/resized/"
 
                 new_name, image = wrangle_image(orig_dir, new_dir)
-                return new_name, image
 
 
         return redirect(url_for("estimate"))
