313b1e78cb212e662f99065e984e01549115fb4f
dakotagporter <dakotap3045@gmail.com>
2021-01-04 18:00:07 -0700

    pep8 fix

--- airbnb_app/wrangler.py
+++ airbnb_app/wrangler.py
@@ -4,7 +4,8 @@ from skimage.transform import resize
 
 
 def wrangle_image(orig_dir, new_dir):
-    image = resize(io.imread(orig_dir), (180, 240), mode='constant', anti_aliasing=True)
+    image = resize(io.imread(orig_dir), (180, 240), mode='constant',
+                   anti_aliasing=True)
 
     if len(image.shape) < 3:
         image = color.grey2rgb(image)
@@ -12,4 +13,3 @@ def wrangle_image(orig_dir, new_dir):
         image = color.rgba2rgb(image)
 
     io.imsave(os.path.join(new_dir, 'test.jpg'), img_as_ubyte(image))
-    # io.imsave(f'{new_dir}/test.jpg', img_as_ubyte(image))
