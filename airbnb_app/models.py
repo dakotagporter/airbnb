f55e66a93dc576d1363f01d2ef37e2b3e93ed7fe
dakotagporter <dakotap3045@gmail.com>
2021-01-05 13:57:53 -0700

    fix upload html

--- airbnb_app/models.py
+++ airbnb_app/models.py
@@ -8,5 +8,7 @@ MIGRATE = Migrate()
 
 class UserInput(DB.Model):
     id = DB.Column(DB.Integer, primary_key=True)
+    name = DB.Column(DB.String(100))
     amenities = DB.Column(DB.PickleType, nullable=False)
+    description = DB.Column(DB.String(1000))
     image = DB.Column(DB.String(300), nullable=False)
