cd2a63decadbba03ec74270200528b7e3fd67119
dakotagporter <dakotap3045@gmail.com>
2021-01-05 10:53:13 -0700

    add user input to db

--- airbnb_app/app.py
+++ airbnb_app/app.py
@@ -6,6 +6,7 @@ from flask_sqlalchemy import SQLAlchemy
 from werkzeug.utils import secure_filename
 
 from .wrangler import wrangle_image
+from .models import UserInput
 
 DB = SQLAlchemy()
 USERDB = SQLAlchemy()
@@ -88,7 +89,7 @@ def create_app():
         Evaluate user input to return estimate.
         """
         if request.method == "POST":
-            amens = request.form["amenities"]
+            amens = request.form.get("amenities")
             if not amens:
                 flash("Please select at least one amenity")
                 return redirect(url_for("upload"))
@@ -103,16 +104,21 @@ def create_app():
                                         str(filename))
                 img.save(orig_dir)
                 new_dir = "images/resized/"
-
                 new_name, image = wrangle_image(orig_dir, new_dir)
 
+            new_input = UserInput(amens, path)
+            os.remove(path)
+
+            DB.session.add(new_input)
+            DB.session.commit()
 
         return redirect(url_for("estimate"))
 
     @app.route("/estimate")
-    def prediction(results=None):
+    def estimate(results=None):
         if results:
-            message = "congrats"
+            amenities = UserInput.query.filter_by(id=id)
+            message = f"Your amenities: {amenities}"
         else:
             message = "No results have been calculated yet!!"
 
